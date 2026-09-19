e07f7e43b6a78306f12dce386e242652198a816e
seregannj <seregannj@gmail.com>
2026-09-19 17:20:47 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ru.yacts.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ru.yacts.app"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
